15d2f776af1de1314fc77cb146e4008c9bb59caa
uuidcode <uuidcode@gmail.com>
2019-03-27 10:36:31 +0900

    callSuper

--- src/main/java/com/github/uuidcode/lombok/test/Child.java
+++ src/main/java/com/github/uuidcode/lombok/test/Child.java
@@ -1,10 +1,14 @@
 package com.github.uuidcode.lombok.test;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @Data(staticConstructor = "of")
 @Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Child extends Parent<Child> {
     private String name;
 }
