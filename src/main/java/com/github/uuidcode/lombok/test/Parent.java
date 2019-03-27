ef806e423e568fa0c698ef2953ecfdfaa057841f
uuidcode <uuidcode@gmail.com>
2019-03-27 10:41:11 +0900

    callSuper

--- src/main/java/com/github/uuidcode/lombok/test/Parent.java
+++ src/main/java/com/github/uuidcode/lombok/test/Parent.java
@@ -3,7 +3,7 @@ package com.github.uuidcode.lombok.test;
 import lombok.Data;
 
 @Data
-public class Parent<T> {
+public abstract class Parent<T> {
     private String id;
 
     public T setId(String id) {
